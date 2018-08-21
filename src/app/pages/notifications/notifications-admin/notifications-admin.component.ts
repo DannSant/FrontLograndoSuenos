fa4ef6af32cc19cb75462ae4283f400999522411
Daniel <dann.santlar@gmail.com>
2018-08-20 23:57:32 -0500

    commit de 20/08/201823:57:31.89

--- src/app/pages/notifications/notifications-admin/notifications-admin.component.ts
+++ src/app/pages/notifications/notifications-admin/notifications-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../../../services/notification.service';
+import { AlertService } from '../../../services/alert.service';
 
 @Component({
   selector: 'app-notifications-admin',
@@ -6,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class NotificationsAdminComponent implements OnInit {
+  notifications:Notification[] = [];
+  filter:any = {};
+  
+  //parametros de busqueda
+  status:number;
+  user:string;
+  isBroadcast:boolean;
 
-  constructor() { }
+  //paginacion
+  fromPag:number=0;
+
+  constructor(
+    public _notifications:NotificationService,
+    public _alert:AlertService
+  ) { }
 
   ngOnInit() {
+    this.populateNotifications();
   }
+
+  populateNotifications(){
+    this.buildFilterObject();
+    
+    this._notifications.getNotifications(this.filter,this.fromPag).subscribe((resp:any)=>{
+     
+      if(resp.ok){
+        this.notifications=resp.data;
+      }else {
+       this._alert.showAlert("Error","Hubo un problema al cargar la informacion","error");
+      }
+    });
+  }
+
+  buildFilterObject(){
+    if(this.isBroadcast){
+      this.filter.broadcast=true;
+      this.filter.user=null;      
+    }else {
+      this.filter.broadcast=false;
+      if(this.user){
+        this.filter.user = this.user;
+      }else {
+        this.filter.user=null;  
+      }      
+    }
+
+    if(this.status==1){
+      this.filter.status=null;
+    }else if(this.status==2){
+      this.filter.status="true";
+    }else if(this.status==3){
+      this.filter.status="false";
+    }
+  }
+
+  resetFilter(){
+    this.filter={};
+  }
+
+
+
+
 
 }
--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AlertService } from './alert.service';
+import { SERVICE_URL } from '../config/config';
+import { UserService } from './user.service';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class NotificationService {
 
-  constructor() { }
+  constructor(
+    public http:HttpClient,
+    public _alert:AlertService,
+    public _userService:UserService
+  ) { }
+
+  getNotifications(filter:any,page:number){
+    let url = SERVICE_URL + `/notification/all?desde=${page}`;
+
+  
+    if(filter.broadcast){
+      url+=`&broadcast=${filter.broadcast}`;
+    }
+    if(filter.user){
+      url+=`&user=${filter.user}`;
+    }
+
+    if(filter.status){
+      url+=`&status=${filter.status}`;
+    }
+
+    let headers = new HttpHeaders({token:this._userService.token})
+
+    return this.http.get(url,{headers}).catch((e)=>{  
+      this._alert.closeWaitWindow();
+      if (!e.error.error){
+        console.log(e); 
+        return
+      }     
+      let errorMessage = e.error.error.message;
+      console.error(errorMessage);      
+      this._alert.showAlert("Error al crear usuario","Ha ocurrido al obtener la informacion, intente nuevamente despues de recargar la pagina","error");
+      
+    
+      return Observable.throw(e);
+    });
+    
+
+  }
 
 }
